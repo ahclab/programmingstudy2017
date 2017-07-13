8c333239720eecda0c5729104e3185a32f70def6
Koichiro Yoshino <mokona1693@hotmail.co.jp>
2017-07-13 09:22:01 +0900

    Revert "Add 2nd class"
    This reverts commit 96583f1d0e0093a2df0357a8ebe2d3ee398f7584.

--- 2-basicprogramming/roop.py
+++
@@ -1,9 +0,0 @@
-my_variable = 5
-
-for i in range (1, my_variable):
-    if i % 3 == 0:
-        print("%d can be devided by 3" % (i))
-    else:
-        print("%d can not be devided by 3" % (i))
-
-        
--- 2-basicprogramming/splitjoin.py
+++
@@ -1,8 +0,0 @@
-my_sentence = "This is a pen"
-print my_sentence
-
-words = my_sentence.split(" ")
-for word in words:
-    print(word)
-
-print("|".join(words))
